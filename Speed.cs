1c4e1e90de8b339c8024b326014549625d9e60ad
Bruce M. Axtens <bugmagnet@outlook.com>
2019-10-24 17:32:18 +0800

    sorted usings

--- Speed.cs
+++ Speed.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RestSharp;
-using Newtonsoft.Json;
+﻿using RestSharp;
+
+using System;
 
 namespace PageSpeed
 {
